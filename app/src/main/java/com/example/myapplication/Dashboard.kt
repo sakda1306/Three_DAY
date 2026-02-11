9d1bdc1a948a319ca258439b8dd13cc1954cab3f
sakda1306 <sakda130646@gmail.com>
2026-02-11 18:09:06 +0700

    sakdaupdate2
    อันล่าสุดนะ
    
    Co-Authored-By: Withun Thaworsin <69392014+9Withun@users.noreply.github.com>

--- app/src/main/java/com/example/myapplication/Dashboard.kt
+++ app/src/main/java/com/example/myapplication/Dashboard.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.List
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
